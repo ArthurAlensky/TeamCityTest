def1a69607fcd6847bb843e1e026376f273d7d02
ArthurAlensky <arthuralensky@gmail.com>
2017-11-16 22:21:27 +0300

    add test

---
+++ TeamCityTest/ServiceTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamCityTest;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var svc = new Service1();
+            var res = svc.GetData(1);
+            Assert.IsNotNull(res, "res is not null");
+        }
+    }
+}
